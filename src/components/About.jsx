e3af2187783994ba1e320ce8dbea001f1c0bf18d
Islam Assanov <islamassanov66@gmail.com>
2024-07-17 08:35:23 +0500

    Changed the text position About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
           transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2 lg:p-8 order-2 lg:order-2"
+          className="w-full lg:w-1/2 lg:p-8 order-2 lg:order-2 lg:mt-10"
         >
           <div className="flex items-center justify-center">
             <p className="my-2 max-w-xl text-xl lg:text-2xl py-6 text-center lg:text-left">{ABOUT_TEXT}</p>
