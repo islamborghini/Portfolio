cba2e9f27fcb3b91553259654ca7303f106dfc6f
Islam Assanov <islamassanov66@gmail.com>
2024-07-17 08:30:10 +0500

    Fixed About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         About
         <span className="text-neutral-500"> Me</span>
       </h1>
-      <div className="flex flex-wrap flex-col">
+      <div className="flex flex-col-reverse lg:flex-row lg:flex-wrap">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
@@ -34,6 +34,13 @@ const About = () => {
               alt="about"
             />
           </div>
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:order-3"
+        >
           <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-4 px-5 mt-4">
             <a
               href="/assets/000AssanovResume.pdf"
