5a8bc910ea151ccc011f4ef5955083325ae89ac9
Islam Assanov <islamassanov66@gmail.com>
2024-07-17 08:32:39 +0500

    Fixed About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,22 +10,12 @@ const About = () => {
         About
         <span className="text-neutral-500"> Me</span>
       </h1>
-      <div className="flex flex-col-reverse lg:flex-row lg:flex-wrap">
+      <div className="flex flex-col lg:flex-row lg:flex-wrap">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
           transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2 lg:p-8 order-2 lg:order-1"
-        >
-          <div className="flex items-center justify-center">
-            <p className="my-2 max-w-xl text-xl lg:text-2xl py-6 text-center lg:text-left">{ABOUT_TEXT}</p>
-          </div>
-        </motion.div>
-        <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
-          transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2 order-1 lg:order-2"
+          className="w-full lg:w-1/2 order-1 lg:order-1"
         >
           <div className="flex items-center justify-center mb-6 lg:mb-0">
             <img
@@ -39,9 +29,12 @@ const About = () => {
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
           transition={{ duration: 0.5 }}
-          className="w-full lg:w-1/2 lg:order-3"
+          className="w-full lg:w-1/2 lg:p-8 order-2 lg:order-2"
         >
-          <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-4 px-5 mt-4">
+          <div className="flex items-center justify-center">
+            <p className="my-2 max-w-xl text-xl lg:text-2xl py-6 text-center lg:text-left">{ABOUT_TEXT}</p>
+          </div>
+          <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-start gap-4 px-5 mt-4">
             <a
               href="/assets/000AssanovResume.pdf"
               target="_blank"
